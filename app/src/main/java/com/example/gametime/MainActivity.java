236ebe22207438d444331e4b7620291d68bd9203
archandatta <archan.datta@carleton.ca>
2020-02-02 12:29:41 -0500

    Notification Underconstruction

--- app/src/main/java/com/example/gametime/MainActivity.java
+++ app/src/main/java/com/example/gametime/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.gametime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationCompat.Builder notificationBuilder;
+    private int currentNotificationID = 0;
+    private NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button m_TestButton = findViewById(R.id.test_button);
+        m_TestButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                // handle the button
+                setDataForSimpleNotification();
+                Toast.makeText(MainActivity.this, "working", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void sendNotification() {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notificationBuilder.setContentIntent(contentIntent);
+        Notification notification = notificationBuilder.build();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        currentNotificationID++;
+        int notificationId = currentNotificationID;
+        if (notificationId == Integer.MAX_VALUE - 1)
+            notificationId = 0;
+         notificationManager.notify(notificationId, notification);
+    }
+
+    private void setDataForSimpleNotification() {
+        notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                //.setLargeIcon(icon)
+                .setContentTitle("Tight")
+                .setContentText("Bro");
+        sendNotification();
     }
 }
