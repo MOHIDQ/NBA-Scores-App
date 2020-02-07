4e97e674091179c7fd1eee46552b5d499f6d16de
mohidqureshi <mohidq99@gmail.com>
2020-02-07 14:40:07 -0500

    Added API Parsing

---
+++ app/src/main/java/com/example/gametime/Game.java
@@ -0,0 +1,108 @@
+package com.example.gametime;
+
+import java.util.Date;
+
+public class Game {
+    private String homeTeam;
+    private String awayTeam;
+
+    private int homeScore;
+    private int awayScore;
+
+    private int quarter; //gonna use status key from JSON
+    //0: Not started
+    //1: The first quarter
+    //2: The second quarter
+    //3: The third quarter
+    //4: The fourth quarter
+    //5: The first OT
+    //6: The second OT
+    //7: The third OT
+    //50: Half-time
+    //-1: Finished
+    //-2: TBD
+    //-3: Interrupted
+    //-4: Cancelled
+    //-5: Postponed
+
+    private String latestPlay;
+
+    private int matchTime; //time game started
+
+    private String quarterTime;
+
+    public Game(String ht, String at, int hs, int as, int q, String lp, int mt, String qt) {
+        homeTeam = ht;
+        awayTeam = at;
+        homeScore = hs;
+        awayScore = as;
+        quarter = q;
+        latestPlay = lp;
+        matchTime = mt;
+        quarterTime = qt;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public void setHomeScore(int homeScore) {
+        this.homeScore = homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    public void setAwayScore(int awayScore) {
+        this.awayScore = awayScore;
+    }
+
+    public int getQuarter() {
+        return quarter;
+    }
+
+    public void setQuarter(int quarter) {
+        this.quarter = quarter;
+    }
+
+    public String getLastPlay() {
+        return latestPlay;
+    }
+
+    public void setLastPlay(String lastPlay) {
+        this.latestPlay = lastPlay;
+    }
+
+    public Date getMatchTime() {
+        return new java.util.Date((long)matchTime*1000);
+    }
+
+    public void setMatchTime(int     matchTime) {
+        this.matchTime = matchTime;
+    }
+
+    public String getQuarterTime() {
+        return quarterTime;
+    }
+
+    public void setQuarterTime(String quarterTime) {
+        this.quarterTime = quarterTime;
+    }
+}
--- app/src/main/java/com/example/gametime/MainActivity.java
+++ app/src/main/java/com/example/gametime/MainActivity.java
@@ -2,13 +2,109 @@ package com.example.gametime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String STRING_URL = "http://api.isportsapi.com/sport/basketball/livescores?api_key=e2inHJ71Nvpk49T8";
+    private ArrayList<Game> currentGameList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        currentGameList = new ArrayList<>();
+
+        if(isNetworkAvailable()) {
+            APICall call = new APICall();
+            call.execute();
+        }
+        //condition for when network connection is not available
+        else {
+            Log.i("TEST", "NO INTERNET");
+        }
+
+    }
+
+    //if network connection is available run async task for getting scores data
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+
+
+
+
+
+
+    //async task for GET requests to REST API
+    private class APICall extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            BufferedReader reader = null;
+            String result = null;
+            StringBuffer sbf = new StringBuffer();
+            String userAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
+            try {
+                URL newUrl = new URL(STRING_URL);
+                HttpURLConnection connection = (HttpURLConnection)newUrl.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setReadTimeout(30000);
+                connection.setConnectTimeout(30000);
+                connection.setRequestProperty("User-agent", userAgent);
+                connection.connect();
+                InputStream is = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                String strRead = null;
+                while ((strRead = reader.readLine()) != null) {
+                    sbf.append(strRead);
+                    sbf.append("\r\n");
+                }
+                reader.close();
+                result = sbf.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            currentGameList = ScoreParser.parseGames(result);
+
+            //for (int i = 0; i < currentGameList.size(); i++) {
+              //  Log.i("TEST", currentGameList.get(i).getAwayTeam());
+            //}
+            //Log.i("TEST", result);
+        }
     }
 }
